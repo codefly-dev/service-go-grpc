0cfa90e8c963d9a336f2dd707519696584dea47c
Antoine Toussaint <toussaint.antoine@gmail.com>
2026-07-30 00:12:32 +0200

    Render deployment environment fixtures (#52)

--- deployment_test.go
+++ deployment_test.go
@@ -9,3 +9,9 @@ import (
 func TestDeploymentTemplates(t *testing.T) {
 	agenttesting.AssertKustomizeTemplates(t, deploymentFS, nil)
 }
+
+func TestAgentVersion(t *testing.T) {
+	if agent.Version != "0.1.25" {
+		t.Fatalf("agent version = %q, want 0.1.25", agent.Version)
+	}
+}
